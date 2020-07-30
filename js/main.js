429a5af783726bf6f42bc7c1941eae6276578d86
Denis <dis-media@rambler.ru>
2020-07-30 21:05:50 +0300

    add media max width 992px

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-document.addEventListener('DOMContentLoader', function () {
 
+var headerSwiper = new Swiper('.header__swiper', {
+  // Optional parameters
+  loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  // parallax: true,
+
+  // spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    progressbarOpposite: true
+  }
 });
+
